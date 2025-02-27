6e3ffef02d85e055e6dc594f3a679c9bf789419b
PonizhenDoRyadovogo <art25ivan@gmail.com>
2025-02-27 23:42:02 +0700

    Изменения: в процессе задумался о том, что, наверное, нам не всегда нужно будет выводить сообщения в консоль, поэтому было сделано следующее
    1. Создан интерфейс IFileMonitor, в котором объявлены чисто виртуальные функции, которые определяются в классе-наследнике ConsoleFileMonitor (в дальнейшем можно будет сделать класс, который выводит сообщения например в файл)
    2. Класс ConsoleFileMonitor является наследником интерфейса IFileMonitor. Этот класс определяет логику вывода сообщений на консоль
    3. Класс FileMonitor изменился, а именно: теперь он хранит указатель на базовый класс IFileMonitor, чтобы вызывать соответствующий метод интерфейса, получив сигнал от FileWatcher

--- fileMonitoring/FileMonitor.cpp
+++ fileMonitoring/FileMonitor.cpp
@@ -2,17 +2,35 @@
 
 #include "FileMonitor.h"
 
-void FileMonitor::onFileExistedAndNotEmpty(const FileInfo &fileInfo)
+void ConsoleFileMonitor::fileExists(const FileInfo &fileInfo)
 {
     std::cout<<"File " << fileInfo.getPath().toStdString() << " exists, size: " << fileInfo.getSize();
 }
 
-void FileMonitor::onFileExistsAndChanged(const FileInfo &fileInfo)
+void ConsoleFileMonitor::fileChanged(const FileInfo &fileInfo)
 {
     std::cout<<"File " << fileInfo.getPath().toStdString() << " changed, size: " << fileInfo.getSize();
 }
 
-void FileMonitor::onFileNotExisted(const QString &filePath)
+void ConsoleFileMonitor::fileDoesNotExist(const QString &filePath)
 {
     std::cout << "File " << filePath.toStdString() << " deleted";
+}
+
+void FileMonitor::onFileExistedAndNotEmpty(const FileInfo &fileInfo)
+{
+    if(m_monitor)
+        m_monitor->fileExists(fileInfo);
+}
+
+void FileMonitor::onFileExistsAndChanged(const FileInfo &fileInfo)
+{
+    if(m_monitor)
+        m_monitor->fileChanged(fileInfo);
+}
+
+void FileMonitor::onFileNotExisted(const QString &filePath)
+{
+    if(m_monitor)
+        m_monitor->fileDoesNotExist(filePath);
 }
